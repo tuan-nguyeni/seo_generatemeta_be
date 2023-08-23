4d0134d15991e392e8c341a0a5762d37206982cb
tuan <tuan.nguyen@datics-consulting.com>
2023-08-23 11:33:47 +0200

    change length of title and desc

--- constants.py
+++ constants.py
@@ -7,9 +7,9 @@ DEFAULT_PORT = 5002
 
 # PROMPT SETTINGS
 PROMPT_FORMAT_TITLE = "I want to generate a title for a website. It should be optimized for SEO and has " \
-                      "less than 580 px. The title should be written in {language}" \
+                      "less than 45 characters. The title should be written in {language}" \
                       " This is the keyword {keyword}  \n\n this is the content: {content} \n\n"
 PROMPT_FORMAT_METADESCRIPTION = "generate a meta description. It should be optimized for SEO and " \
-                                "has less than 960 px. The generated meta description" \
+                                "has less than 120 characters. The generated meta description" \
                                 " should be written in {language}"
 SYSTEM_MESSAGE = "You are an intelligent assistant."
